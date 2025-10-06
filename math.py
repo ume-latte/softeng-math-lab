ac14d47923b83ffb4c8998d91e2305adf9c8552a
b-sugar LaTTe <natine93@gmail.com>
2025-10-06 23:17:51 +0800

    add Implement fuction

--- math.py
+++ math.py
@@ -13,3 +13,16 @@ elif choice == '/':
 else:
     print("Invalid input")
 
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Cannot divide by zero."
+    return a / b
